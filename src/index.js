a4ec6616ee79587e7c83abfd75d1ce59a1b26107
Anirban Pati <anirbanpati55@gmail.com>
2024-03-27 23:06:50 +0530

    util code,

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({ path: "./env" });
@@ -9,7 +10,21 @@ dotenv.config({ path: "./env" });
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+
+    
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT }`);
+    });
+}).catch((err)=>{
+
+  app.on("error" ,(error)=> {
+    console.log("Error connecting to DB",error);
+    throw error;
+ })
+ console.log("Error connecting to DB",err);
+});
 
 
 
